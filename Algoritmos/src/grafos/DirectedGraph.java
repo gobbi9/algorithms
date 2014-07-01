50820b601302fd828dd71314b11da8c068f007cf
guilherme <gobbi9@live.com>
2014-07-01 12:14:17 -0300

    Adicionada especialização do método link() e a implementação do método loadFromMatrix() da super classe.

--- Algoritmos/src/grafos/DirectedGraph.java
+++ Algoritmos/src/grafos/DirectedGraph.java
@@ -1,53 +1,32 @@
 package grafos;
 
+import algoutil.Util;
+
 public class DirectedGraph extends AbstractGraph<Vertex, DirectedEdge> {
 		
 	public DirectedGraph(){
 		super();
 	}
 
-	public void addVertex(Vertex v) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void removeVertex(Vertex v) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void addEdge(DirectedEdge e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void removeEdge(DirectedEdge e) {
-		// TODO Auto-generated method stub
-		
-	}
-
 	public void link() {
 		if (!linked) {
-			for (Edge edge : edges) {
+			for (DirectedEdge edge : edges) {
 				edge.getA().add(edge.getB());
 			}
 			linked = true;
 		}		
 	}
 
-	public void unlink() {
-		// TODO Auto-generated method stub
-		
-	}
 
-	public GraphElement clone() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public void loadFromAdjacencyMatrix(String fileName) {
-		// TODO Auto-generated method stub
-		
+	public void loadFromMatrix(String fileName) {
+		reset();
+		int[][] matrix = Util.loadMatrixFromFile(fileName);
+		for (int i = 0; i < matrix.length; i++)
+			vertices.add(new Vertex());
+		for (int i = 0; i < matrix.length; i++)
+			for (int j = 0; j < matrix[i].length; j++)
+				if (matrix[i][j] != 0)
+					edges.add(new DirectedEdge(vertices.get(i), vertices.get(j), matrix[i][j]));		
 	}
 	
 }
