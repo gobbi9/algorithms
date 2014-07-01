0cbcf73436df404c8f2c7b2ecbf421468d48dec5
guilherme <gobbi9@live.com>
2014-07-01 12:04:18 -0300

    Removidos métodos que operavam sobre o grafo, eles agora pertencem a superclasse, método loadFromMazeInput agora se chama loadFromMatrix.

--- Algoritmos/src/grafos/SimpleGraph.java
+++ Algoritmos/src/grafos/SimpleGraph.java
@@ -2,83 +2,24 @@ package grafos;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Iterator;
 import java.util.Scanner;
+
+import algoutil.Util;
 
 /**
  * 
- * Pratical Use: @link http://www.graph-magics.com/practic_use.php Tentarei
- * fazer o "shortest path"
+ * Pratical Use: @link http://www.graph-magics.com/practic_use.php Tentarei fazer o "shortest path"
  */
 
 // TODO metodo equals
 // TODO metodo clone deve fazer um "deepclone"
-public class SimpleGraph extends AbstractGraph<Vertex, Edge>{
+public class SimpleGraph extends AbstractGraph<Vertex, Edge> {
 
 	public SimpleGraph() {
 		super();
 	}
-	
-	public void addVertex(Vertex newVertex) {
-		if (linked) {
-			// TODO
-		} else {
-			for (Vertex v : vertices)
-				if (v.equals(newVertex)) {
-					// debug
-					System.out.printf("Vertex %s já existe.", newVertex);
-					return;
-				}
-			vertices.add(newVertex);
-		}
-	}
 
-	public void addEdge(Edge newEdge) {
-		if (linked) {
-			//TODO
-		} 
-		else {
-			for (Edge e : edges) {
-				if (e.equals(newEdge)) {
-					// debug
-					System.out.printf("Edge %s já existe.\n",newEdge);
-					return;
-				}
-			}
-		}
-
-		edges.add(newEdge);
-	}
-
-	public void removeVertex(Vertex v) {
-		if (linked) {
-			// TODO mais complicado
-		} else {
-			//JAVA 8 testar para o caso de nao achar 
-			// edges.removeIf(edge -> edge.contains(v));
-			Iterator<Edge> iterator = edges.iterator();
-			while(iterator.hasNext()){
-				Edge edge = iterator.next();
-				if (edge.contains(v))
-					iterator.remove();
-			}
-			vertices.remove(v);
-		}
-	}
-
-	public void removeEdge(Edge edge) {
-		if (linked) {
-			// TODO mais complicado ainda :/
-		} else {
-			edges.remove(edge);
-		}
-	}
-
-	public void unlink() {
-		//TODO
-	}
-			
-	public void loadFromSimpleInput(String fileName){
+	public void loadFromSimpleInput(String fileName) {
 		reset();
 		// Le um arquivo no formato:
 		// V E (numero de vertexes/numero de edges)
@@ -96,7 +37,8 @@ public class SimpleGraph extends AbstractGraph<Vertex, Edge>{
 			input = new File(fileName);
 			scan = new Scanner(input);
 
-		} catch (FileNotFoundException e) {
+		}
+		catch (FileNotFoundException e) {
 			e.printStackTrace();
 			System.exit(-1);
 		}
@@ -108,8 +50,8 @@ public class SimpleGraph extends AbstractGraph<Vertex, Edge>{
 		// inclui #iVertex's vertices no graph
 		for (int i = 0; i < iVertex; i++)
 			// todos em i,0 por enquanto
-			addVertex(new Vertex());		
-	
+			addVertex(new Vertex());
+
 		// indices de dois vertices v1 e v2
 		int v1, v2;
 		for (int i = 0; i < iEdges; i++) {
@@ -120,27 +62,26 @@ public class SimpleGraph extends AbstractGraph<Vertex, Edge>{
 			Vertex a = getVertex(v1);
 			Vertex b = getVertex(v2);
 			// inclui a edge
-			addEdge(new Edge(a,b));
+			addEdge(new Edge(a, b));
 		}
-		
+
 		// encerra o scanner
 		scan.close();
-		
 
 	}
-	
-	//TODO
-	public void loadFromAdjacencyMatrix(String fileName){
-		
-	}
 
-	@Override
-	public GraphElement clone() {
-		// TODO Auto-generated method stub
-		return null;
+	public void loadFromMatrix(String fileName) {
+		reset();
+		int[][] matrix = Util.loadMatrixFromFile(fileName);
+		for (int i = 0; i < matrix.length; i++)
+			vertices.add(new Vertex());
+		for (int i = 0; i < matrix.length; i++)
+			for (int j = 0; j < matrix[i].length; j++)
+				if (j > i && matrix[i][j] != 0)
+					edges.add(new Edge(vertices.get(i), vertices.get(j), matrix[i][j]));
+
 	}
 
 	// -------------- getters and setters --------------------//
-
 
 }
