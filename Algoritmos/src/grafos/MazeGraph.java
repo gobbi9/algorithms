7027b6db6827c0384b708a9749fedcd53d832e75
guilherme <gobbi9@live.com>
2014-07-01 13:20:11 -0300

    Adicionado método que retorna a matrix do labirinto a partir dos vértices do MazeGraph.

--- Algoritmos/src/grafos/MazeGraph.java
+++ Algoritmos/src/grafos/MazeGraph.java
@@ -6,55 +6,81 @@ import java.util.stream.Collectors;
 
 import algoutil.Util;
 
-public class MazeGraph extends AbstractGraph<MazeVertex, MazeEdge>{
+public class MazeGraph extends AbstractGraph<MazeVertex, MazeEdge> {
 
-	public void loadFromMatrix(String fileName){
+	private int width, height;
+
+	public void loadFromMatrix(String fileName) {
 		super.reset();
 
 		int[][] matrix = Util.loadMatrixFromFile(fileName);
 
-		for (int i = 0; i < matrix.length; i++) {
-			for (int j = 0; j < matrix[i].length; j++) {
+		width = matrix.length;
+		height = matrix[0].length;
+
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
 				VertexType t;
-				switch(matrix[i][j]){
-					case 0: t = VertexType.FLOOR;
+				switch (matrix[i][j]) {
+					case 0:
+						t = VertexType.FLOOR;
 						break;
-					case 1: t = VertexType.WALL;
+					case 1:
+						t = VertexType.WALL;
 						break;
-					case 2: t = VertexType.START;
+					case 2:
+						t = VertexType.START;
 						break;
-					case 3: t = VertexType.END;
+					case 3:
+						t = VertexType.END;
 						break;
-					default: t = VertexType.FLOOR;
+					default:
+						t = VertexType.FLOOR;
 						break;
 				}
-				vertices.add(new MazeVertex(i,j,t));
+				vertices.add(new MazeVertex(i, j, t));
 			}
 		}
-		
+
 		List<int[]> possibleConnections;
 		MazeVertex center, connection;
-		for (int i = 0; i < matrix.length; i++) {
-			for (int j = 0; j < matrix[i].length; j++) {
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
 				final int I = i, J = j;
 				possibleConnections = Util.checkArea(matrix, i, j);
 				possibleConnections.removeIf(v -> matrix[v[0]][v[1]] == VertexType.WALL.getType());
-				try{
-					center = vertices.parallelStream().filter(v -> v.getX() == I && v.getY() == J && 
-							v.getType().getType() != VertexType.WALL.getType()).findAny().get();
+				try {
+					center = vertices
+							.parallelStream()
+							.filter(v -> v.getX() == I && v.getY() == J
+									&& v.getType().getType() != VertexType.WALL.getType()).findAny().get();
 				}
-				catch(NoSuchElementException e){
+				catch (NoSuchElementException e) {
 					continue;
 				}
-				for (int[] vConnect : possibleConnections){
-					connection = vertices.parallelStream().
-							filter(v -> v.getX() == vConnect[0] && v.getY() == vConnect[1]).findAny().get();
+				for (int[] vConnect : possibleConnections) {
+					connection = vertices.parallelStream().filter(v -> v.getX() == vConnect[0] && v.getY() == vConnect[1])
+							.findAny().get();
 					edges.add(new MazeEdge(center, connection));
 				}
 			}
 		}
-		
+
 		edges = edges.stream().distinct().collect(Collectors.toList());
+	}
+
+	public int[][] getMatrix() {
+		int[][] matrix = new int[width][height];
+
+		for (int i = 0; i < width; i++)
+			for (int j = 0; j < height; j++) {
+				final int I = i, J = j;
+				matrix[i][j] = vertices.parallelStream().filter(v -> v.getX() == I && v.getY() == J).findAny().get()
+						.getType().getType();
+			}
+
+		return matrix;
+
 	}
 
 }
