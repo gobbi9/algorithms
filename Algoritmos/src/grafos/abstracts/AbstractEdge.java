44d1bfdc4a553c788d8e6e7e4e860f6edd84c31c
Lucas R <rosalino.lucas@hotmail.com>
2014-07-03 20:49:07 -0300

    incluído método toHtml()

--- Algoritmos/src/grafos/abstracts/AbstractEdge.java
+++ Algoritmos/src/grafos/abstracts/AbstractEdge.java
@@ -6,9 +6,10 @@ import java.util.List;
 public abstract class AbstractEdge<T extends AbstractVertex<T>> extends GraphElement {
 	protected List<T> vertexes;
 	protected int weight;
+	protected static final int MIN_WEIGHT = 1;
 	
 	public AbstractEdge(){
-		this(1);
+		this(MIN_WEIGHT);
 	}
 	
 	public AbstractEdge(int weight) {
@@ -43,6 +44,18 @@ public abstract class AbstractEdge<T extends AbstractVertex<T>> extends GraphEle
 	public String toString() {
 		return String.format("[%s, %s](%d)", vertexes.get(0).toString(),
 				vertexes.get(1).toString(), weight);
+	}
+	
+	public String toHtml() {
+		// retorna uma string no formato:
+		// A -- B[label=x];
+		// onde A e B são os vertices e label é o peso caso não seja o peso padrão 
+		String s = String.format("%c %s %c%s;\n", 
+				getA().toChar(), "--", 
+				getB().toChar(),
+				getWeight() > MIN_WEIGHT ? "[label="+getWeight() : "");
+		
+		return s;
 	}
 	
 	public T getA(){
