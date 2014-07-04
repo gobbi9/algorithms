9435ef2df3fdb5c309cbfe24f0a9bfed51e1d3e7
guilherme <gobbi9@live.com>
2014-07-03 21:35:10 -0300

    toHtml usa id's em vez de chars na visualização

--- Algoritmos/src/grafos/MazeEdge.java
+++ Algoritmos/src/grafos/MazeEdge.java
@@ -27,5 +27,17 @@ public class MazeEdge extends AbstractEdge<MazeVertex> {
 	public MazeEdge clone() {
 		return new MazeEdge(deepCopyList());
 	}
+	
+	public String toHtml() {
+		// retorna uma string no formato:
+		// A -- B[label=x];
+		// onde A e B são os vertices e label é o peso caso não seja o peso padrão 
+		String s = String.format("%d %s %d%s;\n", 
+				getA().getId(), "--", 
+				getB().getId(),
+				getWeight() > MIN_WEIGHT ? "[label="+getWeight()+"]" : "");
+		
+		return s;
+	}
 
 }
