e3d7db555c8dcd9c31bd8819ec91deee0514e32a
guilherme <gobbi9@live.com>
2014-07-06 15:05:50 -0300

    Added attribute 'visited' Now all children of GraphElement have an attribute visited.

--- src/graphs/abstracts/GraphElement.java
+++ src/graphs/abstracts/GraphElement.java
@@ -5,11 +5,20 @@ import graphs.interfaces.Element;
 public abstract class GraphElement implements Element<GraphElement>{
 	protected static int objCounter = 0;
 	protected int id;
-
+	protected boolean visited;
+	
 	protected GraphElement() {
 		id = GraphElement.objCounter++;
 	}
-
+	
+	public void visit() {
+		this.visited = true;
+		// debug
+		System.out.printf("%s visited.\n", toString());
+		return;
+	}
+	
+	
 	public int hashCode(){
 		return id;
 	}
@@ -21,6 +30,14 @@ public abstract class GraphElement implements Element<GraphElement>{
 	
 	public int getId() {
 		return this.id;
+	}
+	
+	public boolean isVisited() {
+		return visited;
+	}
+
+	public void setVisited(boolean visited) {
+		this.visited = visited;
 	}
 
 	public abstract GraphElement clone();
