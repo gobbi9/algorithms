64cc3af3bbe0d6802c5394df91fb6e0902e9397c
guilherme <gobbi9@live.com>
2014-07-13 20:55:25 -0300

    created method to load from an incidence matrix input

--- src/graphs/DirectedGraph.java
+++ src/graphs/DirectedGraph.java
@@ -15,32 +15,35 @@ public class DirectedGraph extends AbstractGraph<Vertex, DirectedEdge> {
 		super();
 	}
 
-	public boolean containsUniversalSink(){
+	public boolean containsUniversalSink() {
 		boolean result = true;
 		int[][] m = getMatrix();
 		int size = m.length;
 		int index = -1;
-		for (int i = 0; i < size; i++){
+		for (int i = 0; i < size; i++) {
 			result = true;
-			for (int j = 0; j < size; j++){
+			for (int j = 0; j < size; j++) {
 				result &= (m[i][j] == 0);
-				if (!result) break;
+				if (!result)
+					break;
 			}
 			if (result)
 				index = i;
 		}
-		if (index == -1) return false;
-		
+		if (index == -1)
+			return false;
+
 		result = true;
-		for (int i = 0; i<size; i++)
-			if (i != index){
+		for (int i = 0; i < size; i++)
+			if (i != index) {
 				result &= (m[i][index] != 0);
-				if (!result) return false;
+				if (!result)
+					return false;
 			}
-				
+
 		return result;
 	}
-	
+
 	public void squareByList() {
 		if (linked) {
 			List<DirectedEdge> edgesToBeAdded = new ArrayList<DirectedEdge>();
@@ -65,7 +68,7 @@ public class DirectedGraph extends AbstractGraph<Vertex, DirectedEdge> {
 		int size = m.length;
 		for (int i = 0; i < size; i++)
 			for (int j = 0; j < size; j++)
-				if(i != j && m[i][j] != 0)
+				if (i != j && m[i][j] != 0)
 					for (int k = 0; k < m[j].length; k++)
 						if (m[j][k] != 0 && i != k)
 							indexesToBeChanged.add(new Index(i, k));
@@ -75,6 +78,34 @@ public class DirectedGraph extends AbstractGraph<Vertex, DirectedEdge> {
 				m[index.i][index.j] = 1;
 		loadFromMatrix(m);
 		link();
+	}
+
+	public void loadFromIncidenceMatrix(String fileName) {
+		loadFromIncidenceMatrix(Util.loadMatrixFromFile(fileName));
+	}
+
+	public void loadFromIncidenceMatrix(int[][] matrix) {
+		reset();
+		int numOfVertexes = matrix.length;
+		int numOfEdges = matrix[0].length;
+
+		for (int i = 0; i < numOfVertexes; i++)
+			vertices.add(new Vertex());
+
+		Vertex a = null, b = null;
+		for (int j = 0; j < numOfEdges; j++) {
+			for (int i = 0; i < numOfVertexes; i++) {
+				if (matrix[i][j] == -1)
+					a = vertices.get(i);
+				if (matrix[i][j] == 1)
+					b = vertices.get(i);
+			}
+			if (a == null || b == null)
+				return;	
+			
+			edges.add(new DirectedEdge(a, b));
+		}
+
 	}
 
 	public void addEdge(DirectedEdge newEdge) {
