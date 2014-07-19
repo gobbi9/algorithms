f8029e671bd238319db851cab69194719f4672b9
guilherme <gobbi9@live.com>
2014-07-19 12:20:57 -0300

    removed abstract method clone

--- src/graphs/interfaces/Element.java
+++ src/graphs/interfaces/Element.java
@@ -3,7 +3,6 @@ package graphs.interfaces;
 public interface Element<T> {
 	public final int START_INDEX = 1;
 	public abstract int getId();
-	public abstract T clone();
 	public abstract int hashCode();
 	public abstract boolean equals (Object e);
 }
