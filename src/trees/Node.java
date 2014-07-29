2cf94f2d8f8ad709bd08e6cdc789ed65565b9bcc
guilherme <gobbi9@live.com>
2014-07-29 19:07:33 -0300

    cleanup

--- src/trees/Node.java
+++ src/trees/Node.java
@@ -30,26 +30,7 @@ public class Node {
 		node.setLevel(this.level + 1);
 		children.add(node);
 	}
-	/*
-	   var nodes = [
-    {id: 1, label: 'Node 1', level: 0}
-    ,{id: 2, label: 'Node 2', level: 1}
-    ,{id: 3, label: 'Node 3', level: 1}
-    ,{id: 4, label: 'Node 4', level: 2}
-    ,{id: 5, label: 'Node 5', level: 2}
-    ,{id: 6, label: 'Node 6', level: 2}
-  ];
 
-  // create an array with edges
-  var edges = [
-    {from: 1, to: 2},
-    {from: 1, to: 3},
-    {from: 2, to: 4},
-    {from: 2, to: 5},
-    {from: 3, to: 6}
-  ];
-	
-	*/
 	public String[] toHtml() {
 		String nodes = "";
 		String edges = "";
